96f944dcd3bff4ab93d528dd7c737163d11bdca8
banglalog71 <km.habibsbd@gmail.com>
2017-10-09 12:37:56 +0600

    v 5 callback function completed

--- start/js/main.js
+++ start/js/main.js
@@ -24,16 +24,42 @@
 // // TweenLite.fromTo(img, 1, { x:-200}, {x:200} );
 // // TweenLite.set(img, { x:-200} ); // set method nirdishto place a object ka set kore.
 // TweenLite.from(img, 1, { x:-200} );
-// TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1} );
+// TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1} ); // autoAlpha: 0, te opacity 0 hoy and visibility none thake.
 
 // })(jQuery);
 // <-----------3/easing-------------->
+// (function($){
+// 	var img = $('img'),
+// 	 h2 = $('h2');
+// // TweenLite.fromTo(img, 1, { x:-200}, {x:200} );
+// // TweenLite.set(img, { x:-200} ); // set method nirdishto place a object ka set kore.
+// TweenLite.from(img, 1, { x:-200, ease:SlowMo.ease} );
+// TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1} );
+
+// })(jQuery);
+
+// <-----------4/Adding Callback functions-------------->
 (function($){
 	var img = $('img'),
-	 h2 = $('h2');
+	 h2 = $('h2'),
+	 i =0;
 // TweenLite.fromTo(img, 1, { x:-200}, {x:200} );
 // TweenLite.set(img, { x:-200} ); // set method nirdishto place a object ka set kore.
-TweenLite.from(img, 1, { x:-200, ease:Power2.easeOut} );
-TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1} );
-
+TweenLite.from(img, 1, { 
+				x:-200, 
+				ease:SlowMo.ease, 
+				onStart: onStart,
+				onUpdate: onUpdate,
+				onComplete: onComplete} ); //ekhane onStart holo function.
+// TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1} );
+function onStart(){
+ console.log('animation started');
+}
+function onUpdate(){
+ //console.log('animation is in progress');
+ h2.text(i++);//ekhane h2 variable text() function add kore shekhane i++ korano hoecha.
+}
+function onComplete(){
+ console.log('animation completed');
+}
 })(jQuery);
