367d2de609e956e18ad4c1476fefcd8e7509b95a
banglalog71 <km.habibsbd@gmail.com>
2017-10-09 14:06:22 +0600

    v7 timeline button
    hola

--- start/js/main.js
+++ start/js/main.js
@@ -89,6 +89,36 @@
 
 
 // <-------#6  /Adding Tweens To A Timeline-------?
+// (function($){
+// 	var img = $('img'),
+// 	 h2 = $('h2'),
+// 	 h1 = $('h1'),
+// 	 intro =$('.intro'),
+// 	 listItem = $('ul li');
+// 	 tl = new TimelineLite();
+//  // // TweenLite.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.2});
+//  // //  TweenLite.from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.4});
+//  // //   TweenLite.from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.6});
+//  // //    TweenLite.from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut, delay: 0.8});
+// 	// // TweenLite.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut, delay:1});
+// 	// tl.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut})
+// 	// .from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, '+=1')
+// 	// .from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 3)//3 second por shuru hobe.
+// 	// .from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 2)
+// 	// .from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 2.5);
+// 	// //timeline a delay use korte hoyna, ektar por arekta amnite chole ashbe.
+// 	// //ekhane '-=0.15' purboborti twin ka over lap kore -0.15s dhara.
+// 	// //+ dile oto second por ashbe. na dile oto shomoy por jeta age ashar age ashbe. 
+// 	tl.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut})
+// 	.add('intro')
+// 	.from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 'intro')//shob intro shate ashbe
+// 	.from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut})//3 second por shuru hobe.
+// 	.from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=3')//intro ashar 3 second por ashbe ei h2
+// 	.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 'intro');
+// })(jQuery);
+
+
+// <-------#7  Controlling Timeline Playback - #7-------?
 (function($){
 	var img = $('img'),
 	 h2 = $('h2'),
@@ -109,10 +139,39 @@
 	// //timeline a delay use korte hoyna, ektar por arekta amnite chole ashbe.
 	// //ekhane '-=0.15' purboborti twin ka over lap kore -0.15s dhara.
 	// //+ dile oto second por ashbe. na dile oto shomoy por jeta age ashar age ashbe. 
-	tl.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut})
+	tl.from(h1, 1, {y:-15, autoAlpha: 0, ease:Power1.easeOut})
 	.add('intro')
-	.from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 'intro')//shob intro shate ashbe
-	.from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut})//3 second por shuru hobe.
-	.from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=3')//intro ashar 3 second por ashbe ei h2
-	.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeOut}, 'intro');
+	.from(intro, 1, {y:-15, autoAlpha: 0, ease:Power1.easeOut})//shob intro shate ashbe
+	.from(img, 1, {y:-15, autoAlpha: 0, ease:Power1.easeOut})//3 second por shuru hobe.
+	.from(h2, 1, {y:-15, autoAlpha: 0, ease:Power1.easeOut})//intro ashar 3 second por ashbe ei h2
+	.from(listItem, 1, {y:-15, autoAlpha: 0, ease:Power1.easeOut});
+	tl.pause();
+
+	$('#btnPlay').on('click',function(){
+		tl.play();
+	});
+	$('#btnPause').on('click',function(){
+		tl.pause();
+	});
+	$('#btnResume').on('click',function(){
+		tl.resume();
+	});
+	$('#btnReverse').on('click',function(){
+		tl.reverse();
+	});
+	$('#btnSpeedUp').on('click',function(){
+		tl.timeScale(8);
+	});
+	$('#btnSlowDown').on('click',function(){
+		tl.timeScale(0.5);
+	});
+	$('#btnSeek').on('click',function(){
+		tl.seek(1);
+	});
+	$('#btnProgress').on('click',function(){
+		tl.progress(0.5);//1 seconder half 50%
+	});
+	$('#btnRestart').on('click',function(){
+		tl.restart();
+	});
 })(jQuery);
